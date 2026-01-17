d5e6cd9e64068d0e908060fe548bb4921490e4eb
7lucky-axolotl228 <7error228@gmail.com>
2026-01-18 00:27:44 +0500

    Import the function to calculate the price of products right

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -27,7 +28,7 @@ cart.forEach((cartItem) => {
                 <div class="cart-item-details">
                     <div class="product-name">${matchingProduct.name}</div>
 
-                    <div class="product-price">$${matchingProduct.priceCents / 100}</div>
+                    <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
 
                     <div class="product-quantity">
                         <span>
